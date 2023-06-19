0c7ca77896f86f38802f9168a927a48ac91a8e56
lemonpuddi <cosmo20020702@gmail.com>
2023-06-19 18:35:44 +0800

    Update minimax.cpp

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -42,7 +42,7 @@ void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
     // Choose a random spot.
-    int depth = 6;
+    int depth = 5;
     //while (true) {
       std::cout << "find start   ";
       auto move = Minimax::get_move(root, depth);
