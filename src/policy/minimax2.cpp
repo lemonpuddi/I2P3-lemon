3a33e29e9b0e72c3a2e6913d9e9c6137dcf0fdce
lemonpuddi <cosmo20020702@gmail.com>
2023-06-19 19:43:00 +0800

    minimax2

---
+++ src/policy/minimax2.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+
+#include "../state/state.hpp"
+#include "./minimax.hpp"
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+Move Minimax::get_move(State *state, int depth){
+  decis root;
+  for (auto it:Construct_tree(state, 0, depth)){
+    root.children.push_back(&it);
+  }
+  int ans = 0;
+  Move ans_move;
+  State next;
+  if(!state->legal_actions.size())
+    state->get_legal_actions();
+  for (auto action: state->legal_actions){
+    next = *state->next_state(action);
+    int tmp = minimax_dfs(state, 0, depth, -10000000, 10000000);
+    if (tmp > ans){
+      ans = tmp;
+      ans_move = action;
+    }
+  }
+  return ans_move;
+}
+
+int minimax_dfs(State *state, int turn, int depth, int a, int b){
+  if (turn == depth){
+    return state->evaluate();
+  } 
+  int ans = 0;
+  if (!turn % 2) ans = 10000000;
+  if(!state->legal_actions.size())
+    state->get_legal_actions();
+  for (auto action: state->legal_actions){
+    State *next_state = state->next_state(action);
+    if (turn % 2){
+      ans = std::max(ans, minimax_dfs(next_state, turn + 1, depth, a, b));
+      a = std::max(a, ans);
+      if (a >= b)break;
+    }else{
+      ans = std::min(ans, minimax_dfs(next_state, turn + 1, depth, a, b));
+      b = std::min(b, ans);
+      if (b <= a)break;
+    }
+   
+  }
+  return ans;
+}
---
+++ src/policy/minimax2.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "../state/state.hpp"
+
+
+/**
+ * @brief Policy class for random policy, 
+ * your policy class should have get_move method
+ */
+int minimax_dfs(State *,int , int ,int ,int);
+class Minimax{
+public:
+  static Move get_move(State *state, int depth);
+};
