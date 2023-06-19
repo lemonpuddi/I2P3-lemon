093a56e3406d60869badc7a9964767e9877e3537
lemonpuddi <cosmo20020702@gmail.com>
2023-06-19 23:24:32 +0800

    Update state.cpp

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -19,27 +19,26 @@ double State::evaluate(){
   for (int i = 0; i < BOARD_H; i++){
     for (int j = 0; j< BOARD_W; i++){
       switch (now.board[0][i][j]){
-        case '1':val += 2*weight.pawn[i][j] ;break;
-        case '2':val += 6*weight.rook[i][j];break;
-        case '3':val += 7*weight.knight[i][j];break;
-        case '4':val += 8*weight.bishop[i][j];break;
-        case '5':val += 20*weight.queen[i][j];break;
-        case '6':val += 2000*weight.king[i][j];break;
+        case '1':val += (2*weight.pawn[i][j]) ;break;
+        case '2':val += (6*weight.rook[i][j]);break;
+        case '3':val += (7*weight.knight[i][j]);break;
+        case '4':val += (8*weight.bishop[i][j]);break;
+        case '5':val += (20*weight.queen[i][j]);break;
+        case '6':val += (2000*weight.king[i][j]);break;
         default:break;
       }
       switch (now.board[1][i][j]){
-        case '1':val -= 2*weight.pawn[BOARD_H-i-1][BOARD_W-j-1] ;break;
-        case '2':val -= 6*weight.rook[BOARD_H-i-1][BOARD_W-j-1];break;
-        case '3':val -= 7*weight.knight[BOARD_H-i-1][BOARD_W-j-1];break;
-        case '4':val -= 8*weight.bishop[BOARD_H-i-1][BOARD_W-j-1];break;
-        case '5':val -= 20*weight.queen[BOARD_H-i-1][BOARD_W-j-1];break;
-        case '6':val -= 2000*weight.king[BOARD_H-i-1][BOARD_W-j-1];break;
+        case '1':val -= (2*weight.pawn[BOARD_H-i-1][BOARD_W-j-1]) ;break;
+        case '2':val -= (6*weight.rook[BOARD_H-i-1][BOARD_W-j-1]);break;
+        case '3':val -= (7*weight.knight[BOARD_H-i-1][BOARD_W-j-1]);break;
+        case '4':val -= (8*weight.bishop[BOARD_H-i-1][BOARD_W-j-1]);break;
+        case '5':val -= (20*weight.queen[BOARD_H-i-1][BOARD_W-j-1]);break;
+        case '6':val -= (2000*weight.king[BOARD_H-i-1][BOARD_W-j-1]);break;
         default:break;
       }
     }
   }
   if (this->player)val = -val;
-  val+=777;
   return val;
 }
 
