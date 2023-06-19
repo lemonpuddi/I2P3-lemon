53fd8658314557c06aafec8fea61de8910922fa4
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 00:25:35 +0800

    Update state.cpp

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -18,25 +18,21 @@ double State::evaluate(){
   Board now = this->board;
   for (int i = 0; i < BOARD_H; i++){
     for (int j = 0; j< BOARD_W; i++){
-      switch (now.board[0][i][j]){
-        return 4;
-        case '1':val += 2*(5+weight.pawn[i][j]) ;break;
-        case '2':val += 6*(5+weight.rook[i][j]);break;
-        case '3':val += 7*(5+weight.knight[i][j]);break;
-        case '4':val += 8*(5+weight.bishop[i][j]);break;
-        case '5':val += 20*(5+weight.queen[i][j]);break;
-        case '6':val += 2000*(5+weight.king[i][j]);break;
-        default:break;
-      }
-      switch (now.board[1][i][j]){
-        case '1':val -= 2*(5+weight.pawn[BOARD_H-i-1][BOARD_W-j-1]) ;break;
-        case '2':val -= 6*(5+weight.rook[BOARD_H-i-1][BOARD_W-j-1]);break;
-        case '3':val -= 7*(5+weight.knight[BOARD_H-i-1][BOARD_W-j-1]);break;
-        case '4':val -= 8*(5+weight.bishop[BOARD_H-i-1][BOARD_W-j-1]);break;
-        case '5':val -= 20*(5+weight.queen[BOARD_H-i-1][BOARD_W-j-1]);break;
-        case '6':val -= 2000*(5+weight.king[BOARD_H-i-1][BOARD_W-j-1]);break;
-        default:break;
-      }
+        if(now.board[0][i][j] == '0'){}
+        else if (now.board[0][i][j] == '1')val += 2*(5+weight.pawn[i][j]);
+        else if (now.board[0][i][j] == '2')val += 6*(5+weight.rook[i][j]);
+        else if (now.board[0][i][j] == '3')val += 7*(5+weight.knight[i][j]);
+        else if (now.board[0][i][j] == '4')val += 8*(5+weight.bishop[i][j]);
+        else if (now.board[0][i][j] == '5')val += 20*(5+weight.queen[i][j]);
+        else if (now.board[0][i][j] == '6')val += 2000*(5+weight.king[i][j]);
+
+        if(now.board[1][i][j] == '0'){}
+        else if (now.board[0][i][j] == '1')val -= 2*(5+weight.pawn[BOARD_H-i-1][BOARD_W-j-1])
+        else if (now.board[0][i][j] == '2')val -= 6*(5+weight.rook[BOARD_H-i-1][BOARD_W-j-1])
+        else if (now.board[0][i][j] == '3')val -= 7*(5+weight.knight[BOARD_H-i-1][BOARD_W-j-1])
+        else if (now.board[0][i][j] == '4')val -= 8*(5+weight.bishop[BOARD_H-i-1][BOARD_W-j-1])
+        else if (now.board[0][i][j] == '5')val -= 20*(5+weight.queen[BOARD_H-i-1][BOARD_W-j-1])
+        else if (now.board[0][i][j] == '6')val -= 2000*(5+weight.king[BOARD_H-i-1][BOARD_W-j-1])
     }
   }
   if (this->player)val = -val;
