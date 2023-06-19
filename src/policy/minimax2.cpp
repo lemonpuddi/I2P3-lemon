6c0fb4a5b333c96d6936b0ce6141b7e681396edf
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 00:02:35 +0800

    Update minimax2.cpp

--- src/policy/minimax2.cpp
+++ src/policy/minimax2.cpp
@@ -38,7 +38,6 @@ double minimax_dfs(State *state, int turn, int depth, double a, double b){
   if (turn == depth){
     for(int i = 0;i<turn;i++)std::cout << " ";
     double tmp = state->evaluate();
-    tmp = 3;
     std::cout << state->encode_state();
     std::cout <<std::endl<<  tmp <<" ,leaf node, " << 34 << std::endl;
     return state->evaluate();
