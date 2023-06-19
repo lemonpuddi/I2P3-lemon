662cf853e46d26e5ace74e11d084e6579a5a4503
lemonpuddi <cosmo20020702@gmail.com>
2023-06-19 20:01:36 +0800

    Update minimax.hpp

--- src/policy/minimax.hpp
+++ src/policy/minimax.hpp
@@ -7,7 +7,7 @@
  * your policy class should have get_move method
  */
 std::vector<decis> Construct_tree(State *state, int turn, int depth);
-int minimax_dfs(State *, decis *,int ,int ,int);
+int minimax_dfs(State *, decis *,int ,int, int ,int);
 class Minimax{
 public:
   static Move get_move(State *state, int depth);
