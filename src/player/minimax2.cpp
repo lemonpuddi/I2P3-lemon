7e0bc02a0daa2800ed1d149da1a3c41664832c07
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 17:41:00 +0800

    loop until limit

--- src/player/minimax2.cpp
+++ src/player/minimax2.cpp
@@ -43,7 +43,7 @@ void write_valid_spot(std::ofstream& fout) {
   while(true) {
     // Choose a random spot.
     int depth = 2;
-    //while (true) {
+    while (true) {
       auto move = Minimax2::get_move(root, depth);
       fout << move.first.first << " " << move.first.second << " "\
           << move.second.first << " " << move.second.second << std::endl;
@@ -51,7 +51,7 @@ void write_valid_spot(std::ofstream& fout) {
           << move.second.first << " " << move.second.second << std::endl; 
       depth += 1;
       fout.flush();
-    //}
+    }
     
     // Remember to flush the output to ensure the last action is written to file.
     break;
