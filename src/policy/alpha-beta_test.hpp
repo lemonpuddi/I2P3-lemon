edf43dfe5023edb6659f45aeec23da8c2a147384
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 20:45:50 +0800

    Update alpha-beta_test.hpp

--- src/policy/alpha-beta_test.hpp
+++ src/policy/alpha-beta_test.hpp
@@ -7,7 +7,7 @@
  * your policy class should have get_move method
  */
 double minimax_dfs(State *,int , int ,double ,double);
-class Minimax2{
+class Minimax3{
 public:
   static Move get_move(State *state, int depth);
 };
