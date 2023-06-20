f14739d5046057a8f6d286be54a7451f95a53546
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 15:24:24 +0800

    Update state.cpp

--- src/state/state.cpp
+++ src/state/state.cpp
@@ -25,7 +25,7 @@ double State::evaluate(){
         else if (now.board[0][i][j] == 4)val += 8*(5+weight.bishop[i][j]);
         else if (now.board[0][i][j] == 5)val += 20*(5+weight.queen[i][j]);
         else if (now.board[0][i][j] == 6)val += 2000*(5+weight.king[i][j]);
-        if(now.board[1 - this->player][i][j] == 0);
+        if(now.board[this->player][i][j] == 0);
         else if (now.board[1][i][j] == 1)val -= 2*(5+weight.pawn[BOARD_H-i-1][BOARD_W-j-1]);
         else if (now.board[1][i][j] == 2)val -= 6*(5+weight.rook[BOARD_H-i-1][BOARD_W-j-1]);
         else if (now.board[1][i][j] == 3)val -= 7*(5+weight.knight[BOARD_H-i-1][BOARD_W-j-1]);
