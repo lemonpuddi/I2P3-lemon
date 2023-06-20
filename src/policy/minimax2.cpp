159f1ac20e2bb247a6d57a2082ee14561d009a3d
lemonpuddi <cosmo20020702@gmail.com>
2023-06-20 16:47:23 +0800

    t

--- src/policy/minimax2.cpp
+++ src/policy/minimax2.cpp
@@ -18,33 +18,33 @@ Move Minimax2::get_move(State *state, int depth){
   Move ans_move;
   State *next;
   std::ofstream log("debug.txt");
-  std::cout << depth <<",";
+  //std::cout << depth <<",";
   if(!state->legal_actions.size())
     state->get_legal_actions();
   for (auto action: state->legal_actions){
     next = state->next_state(action);
     int tmp = (int)minimax_dfs(next, 0, depth, -10000000, 10000000);
-    std::cout << tmp << ",AA";
+    //std::cout << tmp << ",AA";
     if (tmp >= ans){
       ans = tmp;
       ans_move = action;
     }
   }
-  std::cout << std::endl;
+  //std::cout << std::endl;
   return ans_move;
 }
 
 double minimax_dfs(State *state, int turn, int depth, double a, double b){
   if (turn == depth){
-    for(int i = 0;i<turn;i++)std::cout << " ";
+    //for(int i = 0;i<turn;i++)std::cout << " ";
     double tmp = state->evaluate();
-    std::cout << state->encode_state();
-    std::cout << tmp <<" ,leaf node, " <<turn<<","<< depth << std::endl;
+    //std::cout << state->encode_state();
+    //std::cout << tmp <<" ,leaf node, " <<turn<<","<< depth << std::endl;
     return state->evaluate();
   } 
-  std::cout << "dfs2 start" << ",turn " << turn;
+  //std::cout << "dfs2 start" << ",turn " << turn;
   double ans = -1000000;
-  std ::cout <<"________________________________________" << std::endl <<state->encode_state();
+  //std ::cout <<"________________________________________" << std::endl <<state->encode_state();
   if (!turn % 2) ans = 1000000;
   if(!state->legal_actions.size())
     state->get_legal_actions();
@@ -55,7 +55,6 @@ double minimax_dfs(State *state, int turn, int depth, double a, double b){
       a = std::max(a, ans);
       //if (a >= b)break;
     }else{
-      std::cout <<"AANANALE";
       ans = std::min(ans, minimax_dfs(next_state, turn + 1, depth, a, b));
       b = std::min(b, ans);
       //if (b <= a)break;
@@ -63,6 +62,6 @@ double minimax_dfs(State *state, int turn, int depth, double a, double b){
    
   }
     for(int i = 0;i<turn;i++)std::cout << " ";
-    std::cout << ans << std::endl;
+    //std::cout << ans << std::endl;
   return ans;
 }
